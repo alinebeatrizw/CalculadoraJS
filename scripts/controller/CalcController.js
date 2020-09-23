cea1014d701a6cf780957cc2446280d20ef5cb07
alinebeatrizw <alinewille31@gmail.com>
2020-09-22 22:17:39 -0300

    add eventos do teclado

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -11,6 +11,7 @@ class CalcController{
         this._currentDate;
         this.initialize();
         this.initButtonsEvents(); 
+        this.initKeyboard();
     }
 
     initialize(){
@@ -24,11 +25,55 @@ class CalcController{
       this.setLastNumberToDisplay();
     
     }
+
+    initKeyboard(){
+        document.addEventListener('keyup', e=>{
+        
+            switch(e.key){
+                case 'Escape': 
+                    this.ClearAll();
+                     break;
+                case 'Backspace': 
+                    this.ClearEntry();
+                    break;
+                case '+': 
+                case '-':
+                case '*':
+                case '/':
+                case '%':
+                    this.addOperation(e.key);
+                    break;
+                case 'Enter': 
+                case '=':
+                    this.calc();
+                    break;
+                case '.':
+                case ',':
+                    this.addDot();
+                    break;
+    
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    this.addOperation(parseInt(e.key));
+                    break;
+            }
+
+        });
+    }
     addEventListenerAll(element, events, fn){
         events.split(' ').forEach(event => {
             element.addEventListener(event, fn, false);
         });
     }
+
 
     ClearAll(){
         this._operation = [];
